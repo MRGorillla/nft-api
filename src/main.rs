aa1652e281a2bcd673638619b58e96b23183fd7f
Abhishek Krishna <hemah0323@gmail.om>
2025-03-26 13:11:34 +0530

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use std::env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set in .env");
 }
